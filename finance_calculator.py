2976ffbcebe9d164f30640fa3948ff02a548f5d0
Catalia20 <imaneettis2@gmail.com>
2024-09-22 23:39:39 +0100

    Create finance_calculator.py

---
+++ finance_calculator.py
@@ -0,0 +1,11 @@
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = imt(input("Enter your total monthly expense: ")
+
+monthly_savings = float(monthly_income) - float(monthly_expens)                       
+                       
+an_interests = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+don = "{:.0f}".format(an_interests)
+
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${don}.")
