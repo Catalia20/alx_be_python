ce49a6b9fa5a2b5008e8d9c8cd7694ba8a6fbd1a
Catalia20 <imaneettis2@gmail.com>
2024-09-22 22:26:08 +0100

    Update match_case_calculator.py

--- match_case_calculator.py
+++ match_case_calculator.py
@@ -1,25 +1,24 @@
-num1 = float(input("Enter the first number:1"))
-num2 = float(input("Enter the second number:2"))
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
 operation = input("choose the operation (+,-,*,/):")
+
 match operation:
-    case '+':
+    case "+":
         result = num1 + num2
         print(f"The result is {result}.")
-    case '-':
+    case "-":
         result = num1 - num2
         print(f"The result is {result}.")
-    case '*':
+    case "*":
         result = num1 * num2
         print(f"The result is {result}.")
-    case '/':
+    case "/":
         if num2 != 0:
             result = num1 / num2
             print(f"The result is {result}.")
             else:
                 print("cannot divide by zero.")
-                case _:
-                    print("Invalid operation.Please choose one of +,-,*,/.")
-                    
+               
             
 
  
