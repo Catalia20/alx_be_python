2d35fff026248c4716cef6ff3cc07be18646f093
Catalia20 <imaneettis2@gmail.com>
2024-09-22 23:28:02 +0100

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+sub = number1 - number2
+multi = number1 * number2
+
+print("addition of ", number1, "number1, "and", number2, "is ", sum)
+print("subtraction of ", number1, "and", number2, "is ", multi)
