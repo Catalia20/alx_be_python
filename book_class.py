2c2a2b0b72526ff144d3761b5c8a5141e94ac3a1
Catalia20 <imaneettis2@gmail.com>
2024-10-13 19:48:45 +0100

    Update book_class.py

--- book_class.py
+++ book_class.py
@@ -1,33 +1,16 @@
 class Book:
-
-    def __init__(self, title: str, author: str, year: int):
-        """
-        Constructor to initialize a Book instance.
-
-        Args:
-            title (str): The title of the book.
-            author (str): The author of the book.
-            year (int): The publication year of the book.
-        """
+    def __init__(self, title: str, author:str, year:int):
         self.title = title
         self.author = author
         self.year = year
 
-    def __del__(self):
-        """
-        Returns a user-friendly string representation of the Book instance.
-
-        Returns:
-            str: A string in the format "(title) by (author), published in (year)".
-        """
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
-    def __repr__(self):
-        """
-        Returns an official string representation of the Book instance,
-        which can be used to recreate the object.
 
-        Returns:
-            str: A string in the format "Book('title', 'author', year)".
-        """
+
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
-    
+
+
+    def __del__(self):
+        ptint(f"Deleting {self.title}")
